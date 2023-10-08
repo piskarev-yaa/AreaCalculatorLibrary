a7fb88bda0d1fc1b795695b49d69f92a52ad93f6
piskarev-yaa <piskarev.yaa@phystech.edu>
2023-10-08 19:13:39 +0300

    SecondTest
    SecondTest

--- AreaCalculatorLibrary/AreaCalculatorLibrary/Triangle.cs
+++ AreaCalculatorLibrary/AreaCalculatorLibrary/Triangle.cs
@@ -68,6 +68,7 @@ namespace AreaCalculatorLibrary
         public Triangle(double firstSide, double secondSide, double thirdSide)
         {
             // TestForGitHubDesktop
+            // SecondTestForGitHubDestop
             FirstSide = firstSide;
             SecondSide = secondSide;
             ThirdSide = thirdSide;
